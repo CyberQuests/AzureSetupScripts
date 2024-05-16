f346971a9b55b95a49cdbe342baa3ede4c127738
Chuck Yang <chuck@ninethreads.com>
2024-05-16 14:31:32 +0800

    Update Windows11Setup.ps1

--- Windows11Setup.ps1
+++ Windows11Setup.ps1
@@ -64,13 +64,10 @@ function Install-Application {
 Install-Application -Uri "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile "$env:TEMP\chrome_installer.exe" -Arguments "/silent /install"
 
 # 安裝 Telegram
-Install-Application -Uri "https://telegram.org/dl/desktop/win64/tsetup-x64.5.0.1.exe" -OutFile "$env:TEMP\telegram_installer.exe" -Arguments "/S"
+Install-Application -Uri "https://telegram.org/dl/desktop/win64/tsetup-x64.5.0.1.exe" -OutFile "$env:TEMP\telegram_installer.exe" -Arguments "/silent /install"
 
 # 安裝 Skype
-Install-Application -Uri "https://go.skype.com/windows.desktop.download" -OutFile "$env:TEMP\skype_installer.exe" -Arguments "/quiet"
-
-# 安裝 WhatsApp
-Install-Application -Uri "https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe" -OutFile "$env:TEMP\whatsapp_installer.exe" -Arguments "/S"
+Install-Application -Uri "https://go.skype.com/windows.desktop.download" -OutFile "$env:TEMP\skype_installer.exe" -Arguments "/silent /install"
 
 # 安裝 Visual Studio Code
 Install-Application -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" -OutFile "$env:TEMP\vscode_installer.exe" -Arguments "/silent /mergetasks=!runcode"
