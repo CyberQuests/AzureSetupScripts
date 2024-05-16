ecbbdc8de34963e9a37f6143a12e6b39d47a0b65
Chuck Yang <chuck@ninethreads.com>
2024-05-16 10:16:51 +0800

    Update Windows11Setup.ps1

--- Windows11Setup.ps1
+++ Windows11Setup.ps1
@@ -36,19 +36,22 @@ function Install-Application {
 }
 
 # 安裝 Google Chrome
-Install-Application -Uri "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile "chrome_installer.exe" -Arguments "/silent /install"
+Install-Application -Uri "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile "$env:TEMP\chrome_installer.exe" -Arguments "/silent /install"
 
 # 安裝 Telegram
-Install-Application -Uri "https://telegram.org/dl/desktop/win64/tsetup-x64.5.0.1.exe" -OutFile "telegram_installer.exe" -Arguments "/S"
+Install-Application -Uri "https://telegram.org/dl/desktop/win64/tsetup-x64.5.0.1.exe" -OutFile "$env:TEMP\telegram_installer.exe" -Arguments "/S"
 
 # 安裝 Skype
-Install-Application -Uri "https://go.skype.com/windows.desktop.download" -OutFile "skype_installer.exe" -Arguments "/quiet"
+Install-Application -Uri "https://go.skype.com/windows.desktop.download" -OutFile "$env:TEMP\skype_installer.exe" -Arguments "/quiet"
+
+# 安裝 WhatsApp
+Install-Application -Uri "https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe" -OutFile "$env:TEMP\whatsapp_installer.exe" -Arguments "/S"
 
 # 安裝 Visual Studio Code
-Install-Application -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" -OutFile "vscode_installer.exe" -Arguments "/silent /mergetasks=!runcode"
+Install-Application -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" -OutFile "$env:TEMP\vscode_installer.exe" -Arguments "/silent /mergetasks=!runcode"
 
 # 安裝 Sandboxie
-Install-Application -Uri "https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.13.7/Sandboxie-Classic-x64-v5.68.7.exe" -OutFile "sandboxie_installer.exe" -Arguments "/S"
+Install-Application -Uri "https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.13.7/Sandboxie-Classic-x64-v5.68.7.exe" -OutFile "$env:TEMP\sandboxie_installer.exe" -Arguments "/S"
 
 # 恢復進度條設置
 $ProgressPreference = $originalProgressPreference
