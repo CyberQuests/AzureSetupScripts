b9885c6d9e8ead83d9e4645417dd06dd58b83446
Chuck Yang <chuck@ninethreads.com>
2024-05-18 15:51:42 +0800

    Update InstallLanguagePack.ps1

--- InstallLanguagePack.ps1
+++ InstallLanguagePack.ps1
@@ -2,7 +2,7 @@
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name LanguagePackManagement -Force
 Import-Module LanguagePackManagement
-Install-Language -Language "zh-TW"
+Install-Language -Language "zh-TW" -CopyToSettings
 Set-WinUILanguageOverride -Language "zh-TW"
 Set-WinSystemLocale "zh-TW"
 Set-WinUserLanguageList "zh-TW" -Force
